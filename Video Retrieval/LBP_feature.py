39d665cf073375ac0f403492a990b428ae9a7961
MuteString <null_string@outlook.com>
2018-03-20 23:09:58 +0800

    实现了LBP特征提取

---
+++ Video Retrieval/LBP_feature.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from skimage.feature import local_binary_pattern
+import scipy.io
+import cv2
+import numpy as np
+
+# Get Mapping from matlab
+mapping = scipy.io.loadmat('mapping.mat')
+mapping['table'] = np.array(mapping['mapping_table']).T
+mapping['samples'] = int(mapping['mapping_samples'])
+mapping['num'] = int(mapping['mapping_num'])
+
+
+def apply_mapping(mat, mapping):
+    bins = mapping['num']
+    result = np.zeros([mat.shape[0], mat.shape[1]])
+    for i in range(mat.shape[0]):
+        for j in range(mat.shape[1]):
+            result[i][j] = mapping['table'][int(mat[i][j])]
+    single_result = []
+    for i in result:
+        single_result.extend(i)
+    result_dict = {k: single_result.count(k)/(mat.shape[0]*mat.shape[1]) for k in set(single_result)}
+    return result_dict
+
+
+def lbp_feature(r, n, image_path):
+    # settings for LBP
+    radius = r
+    n_points = n
+
+    image = cv2.imread(image_path)  # 读取图像
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # 转换为灰度图显示
+    lbp = local_binary_pattern(image, n_points, radius)  # 提取LBP特征
+    lbp_vec = apply_mapping(lbp, mapping)                # 映射特征到特征向量
+    return lbp_vec
